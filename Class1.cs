36f270ab9d6ae872b12bc4ad5f04e028e65bc277
DamSaulGoodMan <capron.damien@laposte.net>
2018-12-24 01:48:24 +0100

    start example User User

--- Class1.cs
+++ Class1.cs
@@ -2,10 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using pGina.Shared.Interfaces;
+using pGina.Shared.Types;
 
 namespace PluginJUmpCloudPgina
 {
-    public class Class1
+    public class JUmpCloudUserAuthentication : pGina.Shared.Interfaces.IPluginAuthentication
     {
+        public string Name
+        {
+            get
+            {
+                return "User";
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "Log user test \"User User\"";
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        //public Guid Uuid => throw new NotImplementedException();
+
+        public Guid Uuid = new Guid("CED8D126-9121-4CD2-86DE-3D84E4A2625E");
+
+        public BooleanResult AuthenticateUser(SessionProperties properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Starting()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stopping()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
