b3901ff8b1a08480e9f39f6542fa4b46e9c8c6ad
Ruben Soetewey <soeteweyruben@gmail.com>
2019-01-18 18:03:41 +0100

    Adding Http Connection

--- Class1.cs
+++ Class1.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using pGina.Shared.Interfaces;
 using pGina.Shared.Types;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.Serialization;
 
 namespace PluginJUmpCloudPgina
 {
@@ -35,7 +39,7 @@ namespace PluginJUmpCloudPgina
 
         //public Guid Uuid => throw new NotImplementedException();
 
-        public Guid Uuid = new Guid("CED8D126-9121-4CD2-86DE-3D84E4A2625E");
+        public Guid Uuid => new Guid("CED8D126-9121-4CD2-86DE-3D84E4A2625E");
 
         public BooleanResult AuthenticateUser(SessionProperties properties)
         {
@@ -50,6 +54,13 @@ namespace PluginJUmpCloudPgina
         public void Stopping()
         {
             throw new NotImplementedException();
+        }
+
+        protected bool PostDemand()
+        {
+            HttpWebRequest client =
+                HttpWebRequest.CreateHttp("https://console.jumpcloud.com/api/v2/activedirectories/");
+            return true;
         }
     }
 }
