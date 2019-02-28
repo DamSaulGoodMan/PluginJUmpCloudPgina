4d446a633b124a1a2f3dc6a2fee2dfbdcb3d3c08
Ruben Soetewey <soeteweyruben@gmail.com>
2019-02-28 15:42:16 +0100

    Cleaning code

--- Class1.cs
+++ Class1.cs
@@ -18,39 +18,9 @@ namespace PluginJUmpCloudPgina
         
         
         public Guid Uuid => new Guid("711B34DE-8404-4C9E-AB1B-56EA62A8FEF0");
-        public void Configure()
-        {
-            throw new NotImplementedException();
-        }
 
-        public BooleanResult AuthenticatedUserGateway(SessionProperties properties)
-        {
-            try
-            {
-                UserInformation userInfo = properties.GetTrackedSingle<UserInformation>();
-                SelectQuery query = new SelectQuery("Win32_UserAccount");
-                ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
-                foreach (ManagementObject envVar in searcher.Get())
-                {
-                    if (envVar["Name"] == userInfo.Username)
-                    {
-                        return new BooleanResult() {Success = true};
-                    }
-                }
-                DirectoryEntry AD = new DirectoryEntry("WinNT://" +  
-                                                       Environment.MachineName + ",computer");  
-                DirectoryEntry NewUser = AD.Children.Add(userInfo.Username, "user");  
-                NewUser.Invoke("SetPassword", new object[] { userInfo.Password });  
-                NewUser.Invoke("Put", new object[] { "Description", "User created by JumpCloud plugin" });  
-                NewUser.CommitChanges();  
-                return new BooleanResult(){Success = true};
-            }
-            catch(Exception e)
-            {
-                return  new BooleanResult(){Success = false,Message =  e.Message};
-            }
-        }
-
+        public string _directory = "5c01746b29f03755c3cfa75e";
+        
         public string Name
         {
             get
@@ -79,14 +49,47 @@ namespace PluginJUmpCloudPgina
         
         #region Methods
         
+        public void Configure()
+        {
+            //TODO : Add configuration's WinForm
+        }
+        
+        public BooleanResult AuthenticatedUserGateway(SessionProperties properties)
+        {
+            try
+            {
+                UserInformation userInfo = properties.GetTrackedSingle<UserInformation>();
+                SelectQuery query = new SelectQuery("Win32_UserAccount");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+                foreach (ManagementObject envVar in searcher.Get())
+                {
+                    if ((string)envVar["Name"] == userInfo.Username)
+                    {
+                        return new BooleanResult() {Success = true};
+                    }
+                }
+                DirectoryEntry AD = new DirectoryEntry("WinNT://" +  
+                                                       Environment.MachineName + ",computer");  
+                DirectoryEntry NewUser = AD.Children.Add(userInfo.Username, "user");  
+                NewUser.Invoke("SetPassword", new object[] { userInfo.Password });  
+                NewUser.Invoke("Put", new object[] { "Description", "User created by JumpCloud plugin" });  
+                NewUser.CommitChanges();  
+                return new BooleanResult(){Success = true};
+            }
+            catch(Exception e)
+            {
+                return  new BooleanResult(){Success = false,Message =  e.Message};
+            }
+        }
+        
         public BooleanResult AuthenticateUser(SessionProperties properties)
         {
             UserInformation userInfo = properties.GetTrackedSingle<UserInformation>();
             string result = SearchUserLDAP(userInfo.Username, userInfo.Password);
             if (result == "Ok")
-                return new BooleanResult() { Success = true ,Message = "ouai" };
+                return new BooleanResult() { Success = true };
             else
-                return new BooleanResult() { Success = false };
+                return new BooleanResult() { Success = false , Message = result};
         }
 
         public void Starting()
@@ -97,13 +100,11 @@ namespace PluginJUmpCloudPgina
 
         private DirectoryEntry createDirectoryEntry(string account,string password)  
         {   
-           //On crée la connection LDAP qui va bien 
-           string directory = "5c01746b29f03755c3cfa75e";
-            DirectoryEntry ldapConnection =
-     new DirectoryEntry(
-         String.Format("LDAP://" +
-                       "ldap.jumpcloud.com:389/ou=Users,o={0},dc=jumpcloud,dc=com", directory));
-            ldapConnection.Username = String.Format("uid={0},ou=Users,o={1},dc=jumpcloud,dc=com",account,directory);
+            //On crée la connection LDAP qui va bien 
+            ;
+            DirectoryEntry ldapConnection = new DirectoryEntry(
+            String.Format("LDAP://ldap.jumpcloud.com:389/ou=Users,o={0},dc=jumpcloud,dc=com", _directory));
+            ldapConnection.Username = String.Format("uid={0},ou=Users,o={1},dc=jumpcloud,dc=com",account,_directory);
             ldapConnection.Password = password;
             ldapConnection.AuthenticationType = AuthenticationTypes.ReadonlyServer;
             return ldapConnection;  
@@ -114,21 +115,19 @@ namespace PluginJUmpCloudPgina
             DirectoryEntry myLdapConnection = createDirectoryEntry(username, password);
             try
             {
-                
                 DirectorySearcher search = new DirectorySearcher(myLdapConnection);
                 //On lance la requête
                 SearchResult result = search.FindOne();
                 //Ici , si on n'est pas tombé dans le catch, c'est que le user/psswd sont Ok
                 if (result != null)
                     return "Ok";
-                Console.Write("kokokokoko");
                 return "Ko";
             }
             catch (Exception e)
             {
-                //Authentification KO
+                //Authentification KO, on affiche le message d'erreur
                 Console.Write(e.Message);
-                return e.Message + "Username = "+username + " Password = " + password + " Connection = "+myLdapConnection.Path+" User = " +myLdapConnection.Username;
+                return e.Message + " Data used : Username = "+username + " Password = " + password + " Connection = "+myLdapConnection.Path+" User = " +myLdapConnection.Username;
             }
 
         }
